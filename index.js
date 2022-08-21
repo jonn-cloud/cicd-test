792be175135af6596e7f1a1876ce9d4df67a1cd5
jonn-cloud <jonathan@cytwist.com>
2022-08-21 15:06:39 +0300

    changed origin https

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const PORT = 8080;
 
 app.get('/', (req, res)=> {
-    res.status(200).send('this is a api')
+    res.status(200).send('this is a api!')
 })
 
 app.listen(PORT)
